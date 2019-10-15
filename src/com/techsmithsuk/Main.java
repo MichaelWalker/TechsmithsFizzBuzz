6f4d95923c9058267762d7e8c5d973c60c37f9fc
Mike <Walker>
2019-10-15 21:48:34 +0100

    Add Fizzbuzz Rules

--- src/com/techsmithsuk/Main.java
+++ src/com/techsmithsuk/Main.java
@@ -4,7 +4,23 @@ public class Main {
 
     public static void main(String[] args) {
 	    for (int counter = 1; counter <= 100; counter++) {
-	        System.out.println(counter);
+	        System.out.println(getResponse(counter));
         }
+    }
+
+    private static String getResponse(int number) {
+        if (number % 3 == 0 && number % 5 == 0) {
+            return "FizzBuzz";
+        }
+
+        if (number % 3 == 0) {
+            return "Fizz";
+        }
+
+        if (number % 5 == 0) {
+            return "Buzz";
+        }
+
+        return String.format("%d", number);
     }
 }
