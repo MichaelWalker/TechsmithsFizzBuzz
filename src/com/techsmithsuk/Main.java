67a4334c2fd17261b6d290e0c5d382002ece4a8f
Mike <Walker>
2019-10-16 17:12:38 +0100

    Refactor out options class.

--- src/com/techsmithsuk/Main.java
+++ src/com/techsmithsuk/Main.java
@@ -1,82 +1,33 @@
 package com.techsmithsuk;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class Main {
-
-    private static List<Rule> DEFAULT_RULES = Arrays.asList(
-        new Rule(3, "Fizz"),
-        new Rule(5, "Buzz"),
-        new Rule(7, "Bang"),
-        new Rule(13, "Fezz")
-    );
-
     public static void main(String[] args) {
-        List<Rule> rules = getRulesFromArgs(args);
-        Integer reverse = getReverseFactorFromArgs(args);
-        String dominant = getDominantSectionFromArgs(args);
+        Options options = new Options(args);
         IntStream.rangeClosed(1, 300)
-                .mapToObj(number -> getResponse(number, rules, reverse, dominant))
+                .mapToObj(number -> getResponse(number, options))
                 .forEach(System.out::println);
     }
 
-    private static List<Rule> getRulesFromArgs(String[] args) {
-        // Expects an input of the form `--rules=3=>Fizz,5=>Buzz`
-
-        List<Rule> commandLineRules = Arrays.stream(args)
-                .filter(arg -> arg.startsWith("--rules="))
-                .map(arg -> arg.replace("--rules=", ""))
-                .flatMap(rulesString -> Stream.of(rulesString.split(",")))
-                .map(Rule::fromString)
-                .collect(Collectors.toList());
-
-        return commandLineRules.isEmpty() ? DEFAULT_RULES : commandLineRules;
-    }
-
-    private static Integer getReverseFactorFromArgs(String[] args) {
-        // Expects an input of the form `--reverse=17`
-        for (String arg : args) {
-            if (arg.startsWith("--reverse=")) {
-                String value = arg.replace("--reverse=", "");
-                return Integer.parseInt(value);
-            }
-        }
-        return null;
-    }
-
-    private static String getDominantSectionFromArgs(String[] args) {
-        // Expects an input of the form `--dominant=11`
-        for (String arg : args) {
-            if (arg.startsWith("--dominant=")) {
-                return arg.replace("--dominant=", "");
-            }
-        }
-        return null;
-    }
-
-    private static String getResponse(Integer number,
-                                      List<Rule> rules,
-                                      Integer reverseFactor,
-                                      String dominantSection) {
-        List<String> sections = rules.stream()
+    private static String getResponse(Integer number, Options options) {
+        List<String> sections = options.getRules().stream()
                 .filter(rule -> rule.matches(number))
                 .map(Rule::getLabel)
                 .collect(Collectors.toList());
 
-        if (reverseFactor != null && number % reverseFactor == 0) {
+        if (options.getReverseFactor() != null && number % options.getReverseFactor() == 0) {
             Collections.reverse(sections);
         }
 
-        if (dominantSection != null) {
-            sections.sort((a, b) -> Comparisons.isDominant(a, b, dominantSection));
+        if (options.getDominantSection() != null) {
+            sections.sort((a, b) -> Comparisons.isDominant(a, b, options.getDominantSection()));
         }
 
-        return sections.isEmpty() ? number.toString() : renderOutput(sections, dominantSection);
+        return sections.isEmpty() ? number.toString() : renderOutput(sections, options.getDominantSection());
     }
 
     private static String renderOutput(List<String> sections, String dominantSection) {
---
+++ src/com/techsmithsuk/Options.java
@@ -0,0 +1,67 @@
+package com.techsmithsuk;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Options {
+    private static List<Rule> DEFAULT_RULES = Arrays.asList(
+            new Rule(3, "Fizz"),
+            new Rule(5, "Buzz")
+    );
+
+    private List<Rule> rules;
+    private Integer reverseFactor;
+    private String dominantSection;
+
+    public Options(String[] args) {
+        this.rules = parseRules(args);
+        this.reverseFactor = parseReverseFactor(args);
+        this.dominantSection = parseDominantSection(args);
+    }
+
+    public List<Rule> getRules() {
+        return rules;
+    }
+
+    public Integer getReverseFactor() {
+        return reverseFactor;
+    }
+
+    public String getDominantSection() {
+        return dominantSection;
+    }
+
+    private List<Rule> parseRules(String[] args) {
+        List<Rule> commandLineRules = Arrays.stream(args)
+                .filter(arg -> arg.startsWith("--rules="))
+                .map(arg -> arg.replace("--rules=", ""))
+                .flatMap(rulesString -> Stream.of(rulesString.split(",")))
+                .map(Rule::fromString)
+                .collect(Collectors.toList());
+
+        return commandLineRules.isEmpty() ? DEFAULT_RULES : commandLineRules;
+    }
+
+    private Integer parseReverseFactor(String[] args) {
+        // Expects an input of the form `--reverse=17`
+        for (String arg : args) {
+            if (arg.startsWith("--reverse=")) {
+                String value = arg.replace("--reverse=", "");
+                return Integer.parseInt(value);
+            }
+        }
+        return null;
+    }
+
+    private String parseDominantSection(String[] args) {
+        // Expects an input of the form `--dominant=11`
+        for (String arg : args) {
+            if (arg.startsWith("--dominant=")) {
+                return arg.replace("--dominant=", "");
+            }
+        }
+        return null;
+    }
+}
