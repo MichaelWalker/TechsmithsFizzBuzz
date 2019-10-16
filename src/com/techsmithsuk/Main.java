302e04dc082251862fd51e972653ffc17b4ba217
Mike <Walker>
2019-10-16 13:45:39 +0100

    Allow users to pick the rules.
    Loose the `Fezz` behaviour unless the user is careful :(

--- src/com/techsmithsuk/Main.java
+++ src/com/techsmithsuk/Main.java
@@ -4,22 +4,37 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Main {
 
+    private static List<Rule> DEFAULT_RULES = Arrays.asList(
+        new Rule(3, "Fizz"),
+        new Rule(5, "Buzz"),
+        new Rule(7, "Bang"),
+        new Rule(13, "Fezz")
+    );
+
     public static void main(String[] args) {
-
-        Rule fizzRule = new Rule(3, "Fizz");
-        Rule buzzRule = new Rule(5, "Buzz");
-        Rule bangRule = new Rule(7, "Bang");
-        Rule fezzRule = new Rule(13, "Fezz");
-
-        List<Rule> rules = Arrays.asList(fizzRule, fezzRule, buzzRule, bangRule);
-
+        List<Rule> rules = getRulesFromArgs(args);
         IntStream.rangeClosed(1, 300)
                 .mapToObj(number -> getResponse(number, rules))
                 .forEach(System.out::println);
+    }
+
+    private static List<Rule> getRulesFromArgs(String[] args) {
+        // Expects an input of the form `--rules=3=>Fizz,5=>Buzz`
+
+        List<Rule> commandLineRules = Arrays.stream(args)
+                .filter(arg -> arg.startsWith("--rules="))
+                .map(arg -> arg.replace("--rules=", ""))
+                .flatMap(rulesString -> Stream.of(rulesString.split(",")))
+                .map(Rule::fromString)
+                .collect(Collectors.toList());
+
+        return commandLineRules.isEmpty() ? DEFAULT_RULES : commandLineRules;
     }
 
     private static String getResponse(Integer number, List<Rule> rules) {
--- src/com/techsmithsuk/Rule.java
+++ src/com/techsmithsuk/Rule.java
@@ -1,5 +1,7 @@
 package com.techsmithsuk;
 
+import java.util.List;
+
 public class Rule {
     private final int factor;
     private final String label;
@@ -15,5 +17,14 @@ public class Rule {
 
     public boolean matches(int number) {
         return number % this.factor == 0;
+    }
+
+    public static Rule fromString(String input) {
+        // Expects an input of the form `3=>Fizz`
+        String[] parts = input.split("=>");
+        int factor = Integer.parseInt(parts[0]);
+        String label = parts[1];
+
+        return new Rule(factor, label);
     }
 }
